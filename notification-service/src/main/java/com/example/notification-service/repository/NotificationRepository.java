8c7fa4974f4fc4c28b938d8df7c9dd875638c91a
unknown <yarrakondaka@gmail.com>
2025-07-13 23:52:04 +0530

    Isolated cart-service

--- notification-service/src/main/java/com/example/notification-service/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationservice.repository;
-
-import com.example.notificationservice.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
