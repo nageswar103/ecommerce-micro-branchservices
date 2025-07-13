835b35889ea9327110432ac3f49bf80b36da7ea5
unknown <yarrakondaka@gmail.com>
2025-07-13 23:52:41 +0530

    Isolated discount-service

--- admin-service/src/main/java/com/example/admin-service/repository/AdminUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.adminservice.repository;
-
-import com.example.adminservice.entity.AdminUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
-}
